5918744484f2b24d624cd8c0d8dba056e67ddc1a
hectoristy <hectoristy@hotmail.com>
2022-04-21 19:59:39 -0400

    formato agragado

--- archivos.c
+++ archivos.c
@@ -8,21 +8,14 @@
 
 int main()
 {
+    //variables
     FILE *archivo;
-
-    archivo = fopen("archivo.txt", "a");
-
-    if (archivo == NULL)
-    {
-        printf("No se pudo abrir el archivo");
-        return 1;
-    }
-
-    char nombre[10];
-    char apellido[10];
+    char nombre[10]; 
+    char apellido[10]; 
     char matricula[10];
     int edad;
 
+    //pedir datos
     printf("\nNombre del estudiante: ");
     scanf("%s", nombre);
     printf("Apellido del estudiante: ");
@@ -33,9 +26,17 @@ int main()
     scanf("%d", &edad);
     printf("\n");
 
-    fprintf(archivo, "%s %s %s %d\n", nombre, apellido, matricula, edad);
+    //guardar datos en archivo
+    if(!(archivo = fopen("archivo.txt", "a"))) 
+    {
+        printf("Error al abrir el archivo");
+        exit(0);
+    }else{
+        fprintf(archivo, "%10s   %10s   %10s   %2d\n", nombre, apellido, matricula, edad);
+        fclose(archivo);
 
-    fclose(archivo);
+        printf("Datos guardados en archivo.txt");
+    }
 
     return 0;
     
